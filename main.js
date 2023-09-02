95045570fd97fd88f25377a382e2149e62fa41e9
Vanila14 <139687493+Vanila14@users.noreply.github.com>
2023-09-03 00:09:28 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+    const myAge = 17
+    const goodAge = 18
+
+    if (myAge >= goodAge) {
+    console.log('Вы совершеннолетний')
+    } 
+    else{
+    console.log('Вам еще нет 18')    
+    }
+    
+
